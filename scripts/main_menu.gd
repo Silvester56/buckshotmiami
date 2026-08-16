97cd798925bffa251b442538e3b85abdabed738f
Silvester56 <vargfoad@gmail.com>
2026-08-16 22:17:28 +0200

    Add mask selection scene and game scene

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,8 +1,7 @@
 extends Control
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
 func _on_quit_pressed() -> void:
 	get_tree().quit()
+
+func _on_start_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/mask_selection.tscn")
---
+++ scripts/mask_selection.gd
@@ -0,0 +1,20 @@
+extends Control
+
+@export var MaskButton: PackedScene
+
+var listOfMasks = [
+	{"id": 0, "label": "Richard"},
+	{"id": 1, "label": "Rasmus"},
+	{"id": 2, "label": "Tony"},
+	{"id": 3, "label": "Aubrey"},
+]
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	var newPositionY = 0;
+	for mask in listOfMasks:
+		var maskButton = MaskButton.instantiate()
+		newPositionY = newPositionY + 120
+		maskButton.position.y = newPositionY
+		maskButton.text = mask.label
+		add_child(maskButton)
