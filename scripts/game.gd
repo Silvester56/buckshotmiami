4a990e8d86b9d95ee0d73268723f5457c07048e0
Silvester56 <vargfoad@gmail.com>
2026-08-18 20:06:21 +0200

    Create basic game file script

---
+++ scripts/game.gd
@@ -0,0 +1,20 @@
+extends Node2D
+
+var shells = []
+var roundsConfiguration = [
+	{ "id": 0, "lives": 2, "min": 2, "max": 4, "objects": 0 },
+	{ "id": 1, "lives": 4, "min": 3, "max": 6, "objects": 2 },
+	{ "id": 2, "lives": 6, "min": 4, "max": 8, "objects": 4 },
+]
+var currentRound = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	var roundObject = roundsConfiguration[currentRound]
+	for n in randi_range(roundObject.min, roundObject.max):
+		shells.push_back(randi() % 2 == 0)
+	shells.shuffle()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
