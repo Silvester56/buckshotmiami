4b6c377d4ec0e90e8446513f7cbd6a2466b86150
Silvester56 <vargfoad@gmail.com>
2026-08-19 20:34:56 +0200

    Add health to player and dealer

--- scripts/game.gd
+++ scripts/game.gd
@@ -1,16 +1,22 @@
 extends Node2D
 
+var dealerHealth = 0
+var playerHealth = 0
 var shells = []
 var roundsConfiguration = [
-	{ "id": 0, "lives": 2, "min": 2, "max": 4, "objects": 0 },
-	{ "id": 1, "lives": 4, "min": 3, "max": 6, "objects": 2 },
-	{ "id": 2, "lives": 6, "min": 4, "max": 8, "objects": 4 },
+	{ "id": 0, "health": 2, "min": 2, "max": 4, "objects": 0 },
+	{ "id": 1, "health": 4, "min": 3, "max": 6, "objects": 2 },
+	{ "id": 2, "health": 6, "min": 4, "max": 8, "objects": 4 },
 ]
 var currentRound = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	var roundObject = roundsConfiguration[currentRound]
+	playerHealth = roundObject.health
+	dealerHealth = roundObject.health
+	$DealerHealth.text = str("Health : ", dealerHealth)
+	$PlayerHealth.text = str("Health : ", playerHealth)
 	for n in randi_range(roundObject.min, roundObject.max):
 		shells.push_back(randi() % 2 == 0)
 	shells.shuffle()
